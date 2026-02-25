dc3a9b1393bded2c7c78dc328ec828313dd1fe53
MrAllatta <ericallatta@gmail.com>
2026-02-25 16:14:43 -0500

    Initial commit django project repo

---
+++ apps/core/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import FarmUser
+
+@admin.register(FarmUser)
+class FarmUserAdmin(UserAdmin):
+    pass
+
+# Register your models here.
