11321854fdd8680b18483ed52ca5b7c5d1d174b0
MrAllatta <ericallatta@gmail.com>
2026-03-01 13:00:41 -0500

    changing site name to farm-site

--- farm-site/urls.py
+++ farm-site/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from apps.planning import urls as purls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("planning/", include(purls)),
 ]
