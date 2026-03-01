d2c2433f332f60a47a92a1b476c1501d0c68a8a0
MrAllatta <ericallatta@gmail.com>
2026-03-01 13:13:19 -0500

    sales.urls

---
+++ apps/sales/urls.py
@@ -0,0 +1,20 @@
+"""sales.urls"""
+
+from django.urls import path
+from . import views
+
+app_name = "sales"
+
+urlpatterns = [
+    path("", views.MarketSalesEntryView.as_view(), name="market_entry"),
+    path(
+        "channel/<int:channel_id>/",
+        views.MarketSalesEntryView.as_view(),
+        name="market_entry_channel",
+    ),
+    path(
+        "channel/<int:channel_id>/date/<str:sale_date>/",
+        views.MarketSalesEntryView.as_view(),
+        name="market_entry_date",
+    ),
+]
