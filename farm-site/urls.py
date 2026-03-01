ba43f5a3e1288dc8e9ec2cc030ad32ef3b1fb7dd
MrAllatta <ericallatta@gmail.com>
2026-03-01 14:41:38 -0500

    fixed import statement

--- farm-site/urls.py
+++ farm-site/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from apps.planning import urls as purls
+from planning import urls as purls
 
 urlpatterns = [
     path("admin/", admin.site.urls),
